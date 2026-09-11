f3f940827dc6457fa2931b9aa3a4a27b5fc6c50b
DavidCorp20 <davidarenascorp@gmail.com>
2026-09-11 14:17:46 -0300

    feat(ui): make decision programmer directly accessible

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './RiskIQApp'
+import App from './DecisionEngineLauncher'
 import './styles.css'
 import './density.css'
 import './button-system.css'
