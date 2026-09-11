b99b79dbe676ae09187ac88504db63af548b0839
DavidCorp20 <davidarenascorp@gmail.com>
2026-09-11 12:46:03 -0300

    feat: promote RiskIQ command center

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './AppAdvanced'
+import App from './RiskCommandCenter'
 import './styles.css'
 import './density.css'
 import './button-system.css'
