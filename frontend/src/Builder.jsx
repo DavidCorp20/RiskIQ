947eaffac2742b5fc2246d6cdbef77d9b2b2a18d
DavidCorp20 <davidarenascorp@gmail.com>
2026-09-12 01:27:39 -0300

    feat(governance): surface policy lifecycle control center

--- frontend/src/Builder.jsx
+++ frontend/src/Builder.jsx
@@ -1,9 +1,11 @@
 import PolicyPortfolioLab from './PolicyPortfolioLab'
 import PolicyBacktest from './PolicyBacktest'
+import GovernancePanel from './GovernancePanel'
 
 export default function Builder(){
   return <div className="riskiq-decision-workspace">
     <PolicyPortfolioLab />
     <PolicyBacktest />
+    <GovernancePanel />
   </div>
 }
