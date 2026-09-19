a77171772cea427316ad2b081c8ef6fb2d62709b
DavidCorp20 <davidarenascorp@gmail.com>
2026-09-19 14:46:20 -0300

    refactor(frontend): remove DOM language layer from production entrypoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './RiskOperatingSystem'
 import { RiskIntelligenceProvider } from './RiskIntelligenceProvider'
@@ -8,15 +8,9 @@ import './riskiq-tailwind.css'
 import './riskiq-portfolio-concentration.css'
 import './enterprise-ui-overrides.css'
 
-function LanguageLayer(){
-  useEffect(()=>import('./riskiq-spanish-ui.js').then(({installRiskIQLanguage})=>installRiskIQLanguage()),[])
-  return null
-}
-
 // Single production entrypoint. Navigation state is owned by React.
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LanguageLayer />
     <RiskLucideBridge />
     <RiskIntelligenceProvider>
       <App />
